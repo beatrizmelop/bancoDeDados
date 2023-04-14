9f195e1bb07485baf880b4e0a4d372b455d0471b
Beatriz Melo <116900521+beatrizmelop@users.noreply.github.com>
2023-04-13 21:03:24 -0300

    Delete src directory

--- src/controllers/usuario-controller.js
+++
@@ -1,9 +0,0 @@
-class UsauarioController{
-    static getUsauarioController( app ) {
-        app.get('/Teste', (req, res) =>
-        res.send('Hello World')
-        );
-    }
-}
-
-export default UsauarioController;
